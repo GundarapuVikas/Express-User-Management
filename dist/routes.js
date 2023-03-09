62cb153585f1a435409bbef8e2a1e3efab7b8fe2
Vikas Gundarapu <vikas_gundarapu@epam.com>
2023-03-09 08:40:28 +0000

    final update

--- dist/routes.js
+++ dist/routes.js
@@ -4,9 +4,12 @@ var _require = require("express"),
   Router = _require.Router;
 var controller = require("./controller");
 var router = Router();
-router.get("/", controller.main);
-router.post("/addUser", controller.addUser);
-router.get("/users", controller.getusers);
-router.get("/fetch_user/:id", controller.fetchUserById);
-router.get("/update_user/:id", controller.updateUserDetails);
+router.get("/",controller.main);
+router.post("/addUser",controller.addUser);
+router.get("/users",controller.getusers);
+router.get("/fetch_user/:id",controller.fetchUserById);
+router.get("/update_user/:id",controller.updateUserDetails);
+router.get("/delete_user/:id",controller.deleteUser);
+router.get("/deletedUsers",controller.listOfDeletedUsers);
+router.get("/AutoSuggestUsers/:substring/:limit",controller.suggestUsers)
 module.exports = router;
